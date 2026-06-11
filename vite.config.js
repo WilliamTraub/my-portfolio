0b175ae3e37de5708b117f8f9b35547d2271efb3
WilliamTraub <traub.w@northeastern.edu>
2026-06-11 14:15:35 -0400

    Revert "Set Vite base to '/' for custom domain"
    This reverts commit ae6d99ad1a623181ff7af6d5a47c030b830b42ca.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Use root base so site works from a custom domain (williamtraub.com)
-  base: "/",
-});
+  base: '/my-portfolio/',
+})
