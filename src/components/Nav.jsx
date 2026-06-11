0bca0607eac209622c297a9c2c48feee242d4864
WilliamTraub <traub.w@northeastern.edu>
2026-06-11 14:25:30 -0400

    Revert "chore(nav): add About link to navigation"
    This reverts commit 457ddd275b2c3fe7a280bb0d663b7da9359323a1.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,6 @@ export default function Nav() {
   return (
     <nav className={styles.nav}>
       <div className={styles.inner}>
-        <a href="#about">About</a>
         <a href="#education">Education</a>
         <a href="#experience">Experience</a>
         <a href="#skills">Skills</a>
